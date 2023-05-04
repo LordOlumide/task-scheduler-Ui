ee0811fccd99ba268420b6e1d3b6460dcb043dc0
emjaycodes <justicenwogu8@gmail.com>
2023-05-04 10:01:46 -0700

    added provider

--- lib/components/rotatin_circle.dart
+++ lib/components/rotatin_circle.dart
@@ -17,32 +17,35 @@ class RotatingCircle extends StatelessWidget {
       builder: (context, child) {
         return Transform.rotate(
           angle: circleAnimation.value,
-          child: Container(
-            height: 300,
-            width: 300,
-            // height: SizeConfig.screenHeight * 0.5,
-            // width: SizeConfig.screenWidth * 0.9,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: ktertiaryColor,
-                width: 30,
-              ),
-              borderRadius: BorderRadius.circular(100),
-              // shape: BoxShape.rectangle,
-              color: ksecondaryColor,
-            ),
+          child: GestureDetector(
+            
             child: Container(
-              height: 100,
-
-              // height: SizeConfig.screenHeight * 0.3,
-              // width: SizeConfig.screenWidth * 0.7,
+              height: 300,
+              width: 300,
+              // height: SizeConfig.screenHeight * 0.5,
+              // width: SizeConfig.screenWidth * 0.9,
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: kwhiteColor,
-                  width: 10,
+                  color: ktertiaryColor,
+                  width: 30,
                 ),
-                shape: BoxShape.circle,
+                borderRadius: BorderRadius.circular(100),
+                // shape: BoxShape.rectangle,
                 color: ksecondaryColor,
+              ),
+              child: Container(
+                height: 100,
+          
+                // height: SizeConfig.screenHeight * 0.3,
+                // width: SizeConfig.screenWidth * 0.7,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: kwhiteColor,
+                    width: 10,
+                  ),
+                  shape: BoxShape.circle,
+                  color: ksecondaryColor,
+                ),
               ),
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:task_scheduler/provider/task_provider.dart';
 import 'package:task_scheduler/screens/home.dart';
-
+import 'package:provider/provider.dart';
 import 'screens/statistics.dart';
+import 'screens/task_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +18,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.encodeSans().fontFamily,
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context)=> TaskProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: GoogleFonts.encodeSans().fontFamily,
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home:
+        HomeScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home:
-      HomeScreen(),
     );
   }
 }
